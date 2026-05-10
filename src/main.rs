56c112c884266a87ef29c4c7ae3f8dfe16915658
bitsixfour <tugirasles@gmail.com>
2026-05-10 13:38:14 -0500

    lol guess it doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::error::Error;
 use rss::Channel;
+use reqwest::Client;
 use anyhow::Result;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let chnl: Channel = rss().await?;
-    for channel in chnl.items() {
-        println!("{}", channel.title().unwrap_or("untitled"));
-    }
+    // let chnl: Channel = rss().await?;
+    // for channel in chnl.items() {
+       // println!("{}", channel.title().unwrap_or("untitled"));
+       // }
+    rss().await?;
     Ok(())
 }
 
 
-async fn rss() -> Result<Channel, Box<dyn Error>> {
-    let content = reqwest::get("https://rateyourmusic.com/~wnguyen7/data/rss")
+async fn rss() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::builder()
+        .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36")
+        .build()?;
+
+    let content = client
+        .get("https://rateyourmusic.com/~wnguyen7/data/rss")
+        .send()
         .await?
-        .bytes()
+        .text()
         .await?;
-    let channel = Channel::read_from(&content[..])?;
-    Ok(channel)
+
+    // let channel = Channel::read_from(&content[..])?;
+    println!("{}", content);
+    Ok(())
 }
